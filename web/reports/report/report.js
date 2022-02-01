786fd777f2edb9556940f8cfd6f8c21f5c77f2d2
adlerzz <adlerzz.true@gmail.com>
2022-02-02 00:34:49 +0300

    go reports!

--- web/reports/report/report.js
+++ web/reports/report/report.js
@@ -1,26 +1,44 @@
-const ot = document.querySelector('.outcomes.categories');
-const it = document.querySelector('.incomes.categories');
-const ob = document.querySelector('.button-outcomes');
-const ib = document.querySelector('.button-incomes');
+registerSwitchingGroup('rep_cat', ['.outcomes.categories', '.incomes.categories']);
+registerSwitchingGroup('rep_but', ['.button.button-outcomes', '.button.button-incomes'], 'activate');
 
 function showOutcomes(){
-    it.classList.add('hidden');
-    ot.classList.remove('hidden');
-
-    ib.classList.remove('active');
-    ob.classList.add('active');
+    switchWithinGroup('rep_cat', '.outcomes.categories');
+    switchWithinGroup('rep_but', '.button.button-outcomes');
 }
 
 function showIncomes(){
-    ot.classList.add('hidden');
-    it.classList.remove('hidden');
-
-    ib.classList.add('active');
-    ob.classList.remove('active');
+    switchWithinGroup('rep_cat', '.incomes.categories');
+    switchWithinGroup('rep_but', '.button.button-incomes');
 }
+
+const its = document.querySelectorAll('.incomes.categories>.category');
+const ots = document.querySelectorAll('.outcomes.categories>.category');
 
 function showBrief(){
 
+}
+
+let state = 'expanded';
+function toggle(){
+    switch(state) {
+        case 'collapsed': {
+            console.log('collapsed -> expanded');
+            ots.forEach(el => {
+                el.querySelector('.articles').classList.remove('hidden')
+            });
+
+            state = 'expanded';
+        } break;
+
+        case 'expanded': {
+            console.log('expanded -> collapsed');
+            ots.forEach(el => {
+                el.querySelector('.articles').classList.add('hidden')
+            })
+
+            state = 'collapsed';
+        } break;
+    }
 }
 
 setTimeout( () => showOutcomes(), 0);

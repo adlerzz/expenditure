665da326a0363776df231d4cd1c4d56fdc1e7f9b
adlerzz <adlerzz.true@gmail.com>
2022-02-20 23:54:34 +0300

    do et

--- web/reports/report/report.ts
+++ web/reports/report/report.ts
@@ -1,6 +1,6 @@
 import {registerSwitchingGroup, switchWithinGroup} from '../../scripts/behavior.js';
 import {getData} from '../../scripts/databus.js';
-import {createSector, drawSector, getColorFromPalette, initDraw} from '../../scripts/draw.js';
+import {createSector, drawSector, getColorFromPalette, initDraw, SectorData} from '../../scripts/draw.js';
 
 console.log('Lets do it');
 
@@ -57,30 +57,85 @@ function toggleColExp(){
 }
 document.querySelector('.button-toggle-colexp')!.addEventListener('click', toggleColExp);
 
-function drawBrief(elementId, key){
 
-    const data = getData(key);
-    const {context, canvas, centerX, centerY} = initDraw(elementId);
-    const r = Math.min(centerX, centerY) * 0.4;
-
+function calcBrief(key: string): any {
     const partToDegrees = (part, whole) => Math.round( part *360 / whole);
 
+    const data = getData(key);
     const volumesData = [
         {
             data: data['outcomes'],
-            palette: 'warm',
-            x0: 2 * centerX * (3/8),
-            y0: centerY
+            palette: 'warm'
         },
         {
             data: data['incomes'],
-            palette: 'cold',
-            x0: 2 * centerX * (7/8),
-            y0: centerY
+            palette: 'cold'
+        }
+
+    ];
+
+    const drawableVolumes = volumesData
+        .map( volumeData => ({
+            categories: volumeData.data,
+            sum: volumeData.data
+                .map(category => category.value < 0 ? 0 : +category.value)
+                .reduce( (a,i) => a+i),
+            palette: volumeData.palette
+        }))
+        .filter( (volume) => volume.sum > 0);
+
+    const sectorsDrawingData = drawableVolumes
+
+        .map( volume => {
+            return volume.categories
+                .map(category => ({value: +category.value, text: category.name}))
+                .reduce((steps, category) => {
+                    if (steps.length === 0) {
+                        return [{startStep: 0, endStep: category.value, text: category.text}]
+                    } else {
+                        const prev = steps[steps.length - 1].endStep;
+                        steps.push({startStep: prev, endStep: prev + category.value, text: category.text});
+                        return steps;
+                    }
+                }, [])
+                .map((sector, i) => ({
+                    startAngle: partToDegrees(sector.startStep, volume.sum) - 90,
+                    endAngle: partToDegrees(sector.endStep, volume.sum) - 90,
+                    text: sector.text,
+                    color: getColorFromPalette(volume.palette, i*4)
+                }));
+        });
+    console.log({sectorsDrawingData});
+    return sectorsDrawingData;
+
+
+}
+
+function drawBrief(elementId: string, key: string){
+
+    const data = getData(key);
+    const sectorsDrawingData = calcBrief(key);
+    const {context, canvas, centerX, centerY} = initDraw(elementId);
+    const r = Math.min(centerX, centerY) * 0.4;
+
+    //const partToDegrees = (part, whole) => Math.round( part *360 / whole);
+
+    const volumesData = [
+        {
+            sectors: sectorsDrawingData[0],
+            x0: 2 * centerX * (1/4),
+            y0: 2 * centerY * (6/8)
+        },
+        {
+            sectors: sectorsDrawingData[1],
+            x0: 2 * centerX * (3/4),
+            y0: 2 * centerY * (6/8)
         },
     ];
 
-    const sectorsDrawingData = volumesData
+    console.log({volumesData});
+
+    /*const sectorsDrawingData = volumesData
         .map( volume => [
             volume,
             volume.data
@@ -108,38 +163,64 @@ function drawBrief(elementId, key){
             return volume;
         });
 
-    console.log({'drawingData': sectorsDrawingData});
+    console.log({'drawingData': sectorsDrawingData});*/
 
-    const sectors = sectorsDrawingData
-        .map(volume => {
-            return volume.data.map( (sectorData, index) => {
-                const color = getColorFromPalette(volume.palette, index);
-                return createSector(context, volume.x0 , volume.y0, r, sectorData.startAngle, sectorData.endAngle, 'gray', color)
-            });
+    const drawingData = volumesData
+        .map((volume, c) => {
+            return volume.sectors.map( (sectorData, i) => ({
+                sector: createSector(context, volume.x0 , volume.y0, r, sectorData.startAngle, sectorData.endAngle, 'gray', sectorData.color),
+                bullet: {
+                    bullet: createSector(context, 30 + c * centerX, 25*i + 20, 5, 0, 360, 'gray', sectorData.color),
+                    x: 45 + c*centerX,
+                    y: 25*(i + 1)
+                },
+                text: sectorData.text
+            }))
         })
-        .flatMap(drawingData => drawingData);
+        .flatMap(i => i);
 
-    console.log({"sectors": sectors});
+    console.log({drawingData});
 
     const back = () => {
         context.fillStyle = 'beige';
+        context.strokeStyle = 'black';
         context.fillRect(0,0, 2*centerX, 2*centerY);
+        /*context.moveTo(0, centerY);
+        context.lineTo(2*centerX, centerY);
+        context.moveTo(centerX, 0);
+        context.lineTo(centerX, 2*centerY);
+        context.stroke();*/
+
     }
 
-    const draw = (needShift) => {
+    const draw = (needShift: (SectorData) => boolean) => {
         back();
-        sectors.forEach(sector => {
+        drawingData.forEach( (category, i) => {
             context.save();
-            drawSector(context, sector, () => {
-                if(needShift(sector)) {
-                    context.translate(sector.x0 + sector.ox, sector.y0 + sector.oy)
+            drawSector(context, category.sector, () => {
+                if(needShift(category.sector)) {
+                    context.translate(category.sector.x0 + category.sector.xOffset, category.sector.y0 + category.sector.yOffset)
                     context.scale(1.05, 1.05);
                 } else {
-                    context.translate(sector.x0, sector.y0);
+                    context.translate(category.sector.x0, category.sector.y0);
                 }
             });
+            drawSector(context, category.bullet.bullet, () => {
+                if(needShift(category.sector)) {
+                    context.translate(category.bullet.bullet.x0, category.bullet.bullet.y0);
+                    context.scale(1.4, 1.4);
+                } else {
+                    context.translate(category.bullet.bullet.x0, category.bullet.bullet.y0);
+                }
+            });
+            context.font = '14px sans-serif';
+            context.fillStyle = 'blue';
+            context.fillText(category.text, category.bullet.x, category.bullet.y);
             context.restore();
+
         });
+
+
     }
 
     draw(() => false);

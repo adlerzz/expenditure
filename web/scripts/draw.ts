390e2f29593c2fb34962a2db1e65a04e088fd80b
adlerzz <adlerzz.true@gmail.com>
2022-02-20 23:54:34 +0300

    do et

--- web/scripts/draw.ts
+++ web/scripts/draw.ts
@@ -1,4 +1,10 @@
-export function initDraw(elementId){
+interface DrawData {
+    context: CanvasRenderingContext2D;
+    canvas: HTMLCanvasElement;
+    centerX, centerY: number;
+}
+
+export function initDraw(elementId: string): DrawData {
     const canvas = document.getElementById(elementId)! as HTMLCanvasElement;
     canvas.width = canvas.clientWidth;
     canvas.height = canvas.clientWidth / 2; //canvas.clientHeight;
@@ -7,17 +13,24 @@ export function initDraw(elementId){
 
     const centerX = canvas.width / 2;
     const centerY = canvas.height / 2;
-    return {context, canvas, centerX, centerY};
+    return {context, canvas, centerX, centerY} as DrawData;
 }
 
-export function createSector(ctx, x0, y0, r, as, af, sc, fc) {
-    const angle = af - as;
-    const ras = Math.PI * as / 180.0;
-    const raf = Math.PI * af / 180.0;
+export interface SectorData {
+    path: Path2D;
+    x0, y0: number;
+    xOffset, yOffset: number;
+    strokeColor, fillColor: string;
+}
+
+export function createSector(ctx, x0, y0, r, startAngle, endAngle, strokeColor, fillColor): SectorData {
+    const angle = endAngle - startAngle;
+    const ras = Math.PI * startAngle / 180.0;
+    const raf = Math.PI * endAngle / 180.0;
     const offsetAngle = (ras + raf) / 2.0;
-    const o = r * 0.035 / (raf - ras);
-    const ox = o * Math.cos(offsetAngle);
-    const oy = o * Math.sin(offsetAngle);
+    const offset = r * 0.035 / (raf - ras);
+    const xOffset = offset * Math.cos(offsetAngle);
+    const yOffset = offset * Math.sin(offsetAngle);
     const path = new Path2D();
 
     if(angle !== 0) {
@@ -29,25 +42,27 @@ export function createSector(ctx, x0, y0, r, as, af, sc, fc) {
     }
 
     ctx.restore();
-    return {path, x0, y0, ox, oy, sc, fc};
+    return {path, x0, y0, xOffset, yOffset, strokeColor, fillColor} as SectorData;
 }
 
-export function drawSector(ctx, sector, additional){
+export function drawSector(ctx, sector: SectorData, additional?: () => void): void {
     ctx.save();
 
     //ctx.translate(sector.cx, sector.cy);
     if(additional){
         additional();
+    } else {
+        ctx.translate(sector.x0, sector.y0);
     }
-    ctx.strokeStyle = sector.sc;
-    ctx.fillStyle = sector.fc;
+    ctx.strokeStyle = sector.strokeColor;
+    ctx.fillStyle = sector.fillColor;
     ctx.lineWidth = 1.0;
     ctx.fill(sector.path);
     ctx.stroke(sector.path);
     ctx.restore();
 }
 
-function getRandomColor(){
+function getRandomColor(): string {
     const v = Math.round((Math.random() * 0x1000000)).toString(16).padStart(6, '0');
     return `#${v}`;
 }
@@ -76,7 +91,7 @@ const COLD_PALETTE = [
     'hsl(260, 90%, 30%)',    'hsl(260, 90%, 40%)',    'hsl(260, 90%, 50%)'
 ]
 
-export function getColorFromPalette(palette, index){
+export function getColorFromPalette(palette: string, index: number): string{
     switch(palette){
         case 'warm': {
             return WARM_PALETTE[index % WARM_PALETTE.length];
